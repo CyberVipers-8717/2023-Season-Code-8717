48067db39fcbad2b6c59fdb838fcc60009fcf75e
Alston Rodriguez <alston1399@gmail.com>
2023-03-26 14:20:45 -0700

    Refactored handlePOV and automatic moving of arm

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -6,30 +6,7 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import frc.robot.Constants.ElevatorEncoderConstants;
-
-/*
- * Structure of Elevator class
- * variable declarations x
- *    manual speeds x
- *    auto speeds x
- * constructor x
- * encoders x
- *    get elevator x
- *    get pulley x
- *    zero positions x
- * dealWithPOV   maybe extract to separate joystick object
- *    rest
- *    double player
- *    mid section
- *    high section
- * special auto stuff
- *    rotate arm to position
- *    extend elevator to position
- * stopMotors x
- *    elevator x
- *    pulley x
- */
+import frc.robot.Constants.ElevatorConstants;
 
 public class Elevator {
   public static boolean targetingCube = true;
@@ -38,10 +15,13 @@ public class Elevator {
   private static final double manualRotateUpScale = 0.9;
   private static final double manualExtendScale = 0.35;
   private static final double manualRetractScale = 0.3;
-  private static final double autoRotateDownScale = 0.6;
-  private static final double autoRotateUpScale = 1;
-  private static final double autoExtendScale = 0.55;
-  private static final double autoRetractScale = 0.55;
+
+  private static final double minimumElevatorDifference = 0.5;
+  private static final double minimumPulleyDifference = 1;
+  private static final double whenToScaleElevatorCommand = 15;
+  private static final double whenToScalePulleyCommand = 25;
+  private static final double maximumElevatorCommand = 0.55;
+  private static final double maximumPulleyCommand = 0.85;
 
   private CANSparkMax elevatorMotorL = new CANSparkMax(6, MotorType.kBrushless);
   private CANSparkMax elevatorMotorR = new CANSparkMax(7, MotorType.kBrushless);
@@ -116,14 +96,67 @@ public class Elevator {
     runPulley(manualRotateUpScale);
   }
 
+  /**
+   * Rotates the arm to the specified encoder position.
+   * @param target The encoder position that the pulley motor will run to match.
+   */
+  public void rotateArmTo(double target) {
+    Robot.moveMotorTo(target, getPulleyPosition(), pulleyMotor, minimumPulleyDifference, maximumPulleyCommand, whenToScalePulleyCommand);
+  }
 
+  /**
+   * Extends, or retracts, the elevator to the specified encoder position.
+   * @param target The encoder position that the elevator motors will run to match.
+   */
+  public void runArmTo(double target) {
+    Robot.moveMotorTo(target, getElevatorPosition(), elevatorMotors, minimumElevatorDifference, maximumElevatorCommand, whenToScaleElevatorCommand);
+  }
 
-
-
-
-
-
-
+    /**
+     * Runs the elevator and pulley to preset encoder positions for cones and cubes.
+     * @param pov The POV angle from the controller.
+     * @param targetingCube A boolean indicating if the current target is a cube, false indicates target is a cone.
+     */
+  public void handlePOV(int pov) {
+    switch (pov) {
+      case 0: // up
+        // high grid
+        if (targetingCube) {
+          rotateArmTo(ElevatorConstants.highSP);
+          runArmTo(ElevatorConstants.highSE);
+        } else {
+          rotateArmTo(ElevatorConstants.highTP);
+          runArmTo(ElevatorConstants.highTE);
+        }
+        break;
+      case 90: // right
+        // mid grid
+        if (targetingCube) {
+          rotateArmTo(ElevatorConstants.midSP);
+          runArmTo(ElevatorConstants.midSE);
+        } else {
+          rotateArmTo(ElevatorConstants.midTP);
+          runArmTo(ElevatorConstants.midTE);
+        }
+        break;
+      case 180: // down
+        // double player station
+        if (targetingCube) {
+          rotateArmTo(ElevatorConstants.doubleSP);
+          runArmTo(ElevatorConstants.doubleSE);
+        } else {
+          rotateArmTo(ElevatorConstants.doubleTP);
+          runArmTo(ElevatorConstants.doubleTE);
+        }
+        break;
+      case 270: // left
+        rotateArmTo(ElevatorConstants.restP);
+        runArmTo(ElevatorConstants.restE);
+        break;
+      default:
+        break;
+    }
+  }
 
   /** Calls the stopMotor method of the elevator motors. */
   public void stopElevator() {
@@ -140,188 +173,4 @@ public class Elevator {
     stopElevator();
     stopPulley();
   }
-
-
-
-
-
-    // /**
-    //  * Runs the elevator and pulley to move to the set encoder positions for cones and cubes.
-    //  * @param pov The POV angle from the controller.
-    //  */
-    // public void dealWithPOV(int pov, boolean targetingCube) {
-    //     switch (pov) {
-    //         case 0: // up
-    //             // high grid
-    //             if (targetingCube) {
-    //                 rotateArmTo(ElevatorEncoderConstants.highSP);
-    //                 runElevatorTo(ElevatorEncoderConstants.highSE);
-    //             } else {
-    //                 rotateArmTo(ElevatorEncoderConstants.highTP);
-    //                 runElevatorTo(ElevatorEncoderConstants.highTE);
-    //             }
-    //             break;
-    //         case 90: // right
-    //             // mid grid
-    //             if (targetingCube) {
-    //                 rotateArmTo(ElevatorEncoderConstants.midSP);
-    //                 runElevatorTo(ElevatorEncoderConstants.midSE);
-    //             } else {
-    //                 rotateArmTo(ElevatorEncoderConstants.midTP);
-    //                 runElevatorTo(ElevatorEncoderConstants.midTE);
-    //             }
-    //             break;
-    //         case 180: // down
-    //             // double player
-    //             if (targetingCube) {
-    //                 rotateArmTo(ElevatorEncoderConstants.doubleSP);
-    //                 runElevatorTo(ElevatorEncoderConstants.doubleSE);
-    //             } else {
-    //                 rotateArmTo(ElevatorEncoderConstants.doubleTP);
-    //                 runElevatorTo(ElevatorEncoderConstants.doubleTE);
-    //             }
-    //             break;
-    //         case 270: // left
-    //             // rest
-    //             rotateArmTo(ElevatorEncoderConstants.restE);
-    //             runElevatorTo(ElevatorEncoderConstants.restP);
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // }
-
-
-
-    
-
-    // public void extendArm(double speed) {
-    //     elevatorMotors.set(speed);
-    // }
-    // /**
-    //  * Extends the elevator, moving the hand away from the robot.
-    //  */
-    // public void extendArm() {
-    //     extendArm(elevatorExtendScale);
-    // }
-    // public void retractArm(double speed) {
-    //     elevatorMotors.set(-speed);
-    // }
-    // /**
-    //  * Retracts the elevator, bringing the hand towards the robot.
-    //  */
-    // public void retractArm() {
-    //     retractArm(elevatorRetractScale);
-    // }
-
-    // /**
-    //  * Rotates the elevator arm down with given speed.
-    //  * @param speed The speed at which the motor will spin, value from 0 to 1.
-    //  */
-    // public void rotateArmDown(double speed) {
-    //     pulleyMotor.set(speed);
-    // }
-    // /**
-    //  * Rotates the elevator arm down at preset speed.
-    //  */
-    // public void rotateArmDown() {
-    //     rotateArmDown(rotateArmDownScale);
-    // }
-
-    // /**
-    //  * Rotates the elevator arm up with given speed.
-    //  * @param speed The speed at which the motor will spin, value from 0 to 1.
-    //  */
-    // public void rotateArmUp(double speed) {
-    //     pulleyMotor.set(-speed);
-    // }
-    // /**
-    //  * Rotates the elevator arm up at preset speed.
-    //  */
-    // public void rotateArmUp() {
-    //     rotateArmUp(rotateArmUpScale);
-    // }
-
-
-
-    
-
-    // /**
-    //  * Rotates the arm to the specified encoder position.
-    //  * @param target The encoder position that the pulley motor will rotate to.
-    //  */
-    // public void rotateArmTo(double target) {
-    //     double difference = target - getPulleyPosition();
-    //     double sign = Math.signum(difference);
-    //     double differenceAbs = Math.abs(difference);
-    //     double minimumDifferenceLevel = 1;
-    //     double scaleDifferenceLevel = 30;
-    //     double adjustedLevel = scaleDifferenceLevel-minimumDifferenceLevel;
-    //     double lowestScaledSpeed = 0.2;
-    //     double tempScale = 1;
-    //     if (differenceAbs < minimumDifferenceLevel) {
-    //         stopPulley();
-    //     } else {
-    //         if (differenceAbs < scaleDifferenceLevel) {
-    //             tempScale = map(differenceAbs, minimumDifferenceLevel, adjustedLevel, lowestScaledSpeed, 1);
-    //         }
-    //         if (sign == -1) {
-    //             rotateArmUp(rotateArmUpScale*tempScale);
-    //         } else if (sign == 1) {
-    //             rotateArmDown(rotateArmDownScale*tempScale);
-    //         } else {
-    //             stopPulley();
-    //         }
-    //     }
-    // }
-    
-    // /**
-    //  * Runs the elevator to the specified encoder position.
-    //  * @param target The encoder position that the elevator will extend or retract to.
-    //  */
-    // public void runElevatorTo(double target) {
-    //     double difference = target - getElevatorPosition();
-    //     double sign = Math.signum(difference);
-    //     double differenceAbs = Math.abs(difference);
-    //     double minimumDifferenceLevel = 0.5;
-    //     double scaleDifferenceLevel = 20;
-    //     double adjustedLevel = scaleDifferenceLevel-minimumDifferenceLevel;
-    //     double lowestScaledSpeed = 0.2;
-    //     double tempScale = 1;
-    //     if (differenceAbs < minimumDifferenceLevel) {
-    //         stopElevator();
-    //     } else {
-    //         if (differenceAbs < scaleDifferenceLevel) {
-    //             tempScale = map(differenceAbs, minimumDifferenceLevel, adjustedLevel, lowestScaledSpeed, 1);
-    //         }
-    //         if (sign == -1) {
-    //             retractArm(elevatorRetractScale*tempScale);
-    //         } else if (sign == 1) {
-    //             extendArm(elevatorExtendScale*tempScale);
-    //         } else {
-    //             stopElevator();
-    //         }
-    //     }
-    // }
-
-    // /**
-    //  * Map a double from one range to another.
-    //  * @param x Double to map.
-    //  * @param a Start of first range.
-    //  * @param b End of first range.
-    //  * @param c Start of second range.
-    //  * @param d End of second range.
-    //  * @return The mapped value x.
-    //  */
-    // public double map(double x, double a, double b, double c, double d) {
-    //     return (x-a)/(b-a)*(d-c)+c;
-    // }
-
-
-
-
-
-
-
-
 }
