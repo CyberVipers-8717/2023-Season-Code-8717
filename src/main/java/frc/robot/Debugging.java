45b1fd6c5049a21fc77fcba8a507978fa8a94059
Alston Rodriguez <alston1399@gmail.com>
2023-04-04 21:51:41 -0700

    added size and position to debug tab

--- src/main/java/frc/robot/Debugging.java
+++ src/main/java/frc/robot/Debugging.java
@@ -7,8 +7,8 @@ public class Debugging {
     public static final ShuffleboardTab DebugTab = Shuffleboard.getTab("Debugging");
 
     public static void robotInit() {
-        DebugTab.add("Elevator Info", new Elevator());
-        DebugTab.add("Drivetrain Info", new Drivetrain());
-        DebugTab.add("Gyro Info", new Drivetrain.Gyro());
+        DebugTab.add("Elevator Info", new Elevator()).withPosition(3, 0).withSize(3, 2);
+        DebugTab.add("Drivetrain Info", new Drivetrain()).withPosition(0, 0).withSize(3, 2);
+        DebugTab.add("Gyro Info", new Drivetrain.Gyro()).withPosition(6, 0).withSize(3, 2);
     }
 }
