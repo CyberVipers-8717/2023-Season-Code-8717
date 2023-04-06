493dbdd917a2622aa74c28ed3d9aa6c91b2d6b23
CyberVipers-8717 <cybervipers8717@gmail.com>
2023-04-06 16:12:21 -0700

    extracted elevator constants to elevatorPresets file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,34 +20,6 @@ public class Constants {
     public static final String kDefaultRotation = "none";
     public static final String kAltRotation = "180";
   }
-  public static final class ElevatorConstants {
-    // E indicates elevator (arm extending and retracting)
-    // P indicates pulley (arm rotation)
-    // S indicates cube (Sqaure)
-    // T indicates cone (Triangle)
-    public static final double groundSP = 210;
-    public static final double groundSE = 4;
-    public static final double groundTP = 210;
-    public static final double groundTE = 4;
-    public static final double quickSP = 13;
-    public static final double quickSE = 1.5;
-    public static final double restP = 5;
-    public static final double restE = 1.5;
-    // all cube heights
-    public static final double highSP = 91.5;
-    public static final double highSE = 77.5;
-    public static final double midSP = 73.5;
-    public static final double midSE = 18.5;
-    public static final double doubleSP = 39;
-    public static final double doubleSE = 1;
-    // all cone heights
-    public static final double highTP = 90;
-    public static final double highTE = 91;
-    public static final double midTP = 74.5;
-    public static final double midTE = 41;
-    public static final double doubleTP = 39.7;
-    public static final double doubleTE = 1;
-  }
   public static final class ControllerConstants {
     public static final int USB = 0;
     public static final int X = 1;
