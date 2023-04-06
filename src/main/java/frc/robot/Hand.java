6c82a189b8b8d4432aafd6fa972ef59045a1d76f
Alston Rodriguez <alston1399@gmail.com>
2023-04-05 23:49:24 -0700

    added sendable information for the dashboard

--- src/main/java/frc/robot/Hand.java
+++ src/main/java/frc/robot/Hand.java
@@ -1,16 +1,27 @@
 package frc.robot;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
-public class Hand {
+public class Hand implements Sendable {
+  public static enum State {Open, Closed, Off}
+  public static State currentState = State.Off;
+
   public static final Value openChannel = Value.kForward;
   public static final Value closeChannel = Value.kReverse;
 
   public static Compressor pcmCompressor = new Compressor(PneumaticsModuleType.CTREPCM);
   public static DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 0);
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    //builder.setSmartDashboardType("Hand");
+    builder.addStringProperty("Current state", Hand::getNameOfState, null);
+  }
 
   /** Contains code that will be called when the robot is turned on. */
   public static void robotInit() {
@@ -21,16 +32,36 @@ public class Hand {
   /** Sets the solenoid to the off channel. */
   public static void off() {
     solenoid.set(Value.kOff);
+    currentState = State.Off;
   }
 
   /** Sets the solenoid to the open hand channel. */
   public static void open() {
     solenoid.set(openChannel);
+    currentState = State.Open;
   }
 
   /** Sets the solenoid to the close hand channel. */
   public static void close() {
     solenoid.set(closeChannel);
+    currentState = State.Closed;
+  }
+
+  /**
+   * @return A string indicating the name of the current state of the hand solenoid.
+   * Possible values are "Open", "Closed", and "Off".
+   */
+  public static String getNameOfState() {
+    switch (currentState) {
+      case Open:
+        return "Open";
+      case Closed:
+        return "Closed";
+      case Off:
+        return "Off";
+      default:
+        return "Off";
+    }
   }
 
   /**
