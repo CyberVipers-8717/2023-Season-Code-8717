2487a9d8498adb887a648c479479de7c98161945
Alston Rodriguez <alston1399@gmail.com>
2023-04-05 23:47:07 -0700

    added hand information and javadocs

--- src/main/java/frc/robot/Debugging.java
+++ src/main/java/frc/robot/Debugging.java
@@ -4,11 +4,13 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class Debugging {
-    public static final ShuffleboardTab DebugTab = Shuffleboard.getTab("Debugging");
+  public static final ShuffleboardTab DebugTab = Shuffleboard.getTab("Debugging");
 
-    public static void robotInit() {
-        DebugTab.add("Elevator Info", new Elevator()).withPosition(3, 0).withSize(3, 2);
-        DebugTab.add("Drivetrain Info", new Drivetrain()).withPosition(0, 0).withSize(3, 2);
-        DebugTab.add("Gyro Info", new Drivetrain.Gyro()).withPosition(6, 0).withSize(3, 2);
-    }
+  /** Contains code that will be called when the robot is turned on. */
+  public static void robotInit() {
+    DebugTab.add("Elevator Info", new Elevator()).withPosition(3, 0).withSize(3, 2);
+    DebugTab.add("Drivetrain Info", new Drivetrain()).withPosition(0, 0).withSize(3, 2);
+    DebugTab.add("Gyro Info", new Drivetrain.Gyro()).withPosition(6, 0).withSize(3, 2);
+    DebugTab.add("Hand Info", new Hand()).withPosition(0, 2).withSize(2, 2);
+  }
 }
